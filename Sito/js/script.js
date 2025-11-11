31c1a4827eaf38303d2f48050c9a69c5894f0384
boghisio <manuel.valente@polomanettiporciatti.edu.it>
2025-11-11 12:31:22 +0100

    d

---
+++ Sito/js/script.js
@@ -0,0 +1,47 @@
+function nav(e, color) {
+    var x = document.querySelector("#primo");
+    x.style.backgroundColor = color;
+}
+
+var bcNav = document
+    .querySelector("#a")
+    .addEventListener("mouseover", function (e) {
+        nav(e, "#ffffff");
+    });
+var bcNav = document
+    .querySelector("#a")
+    .addEventListener("mouseout", function (e) {
+        nav(e, "#35424bff");
+    });
+
+function nava(e, color) {
+    var x = document.querySelector("#secondo");
+    x.style.backgroundColor = color;
+}
+
+var bcNav = document
+    .querySelector("#aa")
+    .addEventListener("mouseover", function (e) {
+        nava(e, "#ffffff");
+    });
+var bcNav = document
+    .querySelector("#aa")
+    .addEventListener("mouseout", function (e) {
+        nava(e, "#35424bff");
+    });
+
+function navaa(e, color) {
+    var x = document.querySelector("#terzo");
+    x.style.backgroundColor = color;
+}
+
+var bcNav = document
+    .querySelector("#aaa")
+    .addEventListener("mouseover", function (e) {
+        navaa(e, "#ffffff");
+    });
+var bcNav = document
+    .querySelector("#aaa")
+    .addEventListener("mouseout", function (e) {
+        navaa(e, "#35424bff");
+    });
